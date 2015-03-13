4cf8ebc10bc0678acea83400524bbd379cda628c
admin <admin@admin-PC>
2015-03-13 23:47:26 +0200

    remove unused comments

--- HelloWorld/src/FirstClass.java
+++ HelloWorld/src/FirstClass.java
@@ -3,12 +3,8 @@
  * @author admin
  *
  */
-public class FirstClass {
-	//This is master branch....
-	//All features branch
-	
+public class FirstClass {	
 	public static void main(String[] args) {
-		//comment 5 + 6
 		System.out.println("Hello world!");
 	}
 }
